ae08656ea273fa3e3a0252f272fff0b8e2c6ab75
Endrit Fazliu <endritfazliu9@gmail.com>
2024-08-05 10:32:23 +0000

    added functionality to place major pieces on board

--- board.js
+++ board.js
@@ -1,8 +1,44 @@
 const WHITE_PAWN = "&#x2659;";
+const WHITE_PIECES = ["&#x2656;", "&#x2658;", "&#x2657;", "&#x2655;", "&#x2654;", "&#x2657;", "&#x2658;", "&#x2656;"]
+
 const BLACK_PAWN = "&#x265F;";
+const BLACK_PIECES = ["&#x265C;", "&#x265E;", "&#x265D;", "&#x265B;", "&#x265A;", "&#x265D;", "&#x265E;", "&#x265C;"]
 
 // TODO: User playing as white by default. There will be a switch to toggle this, which will negate this value, and replace the pieces. 
 const isPlayingWhite = true; 
+
+function placePieces() {
+    // Assumes the player is playing white as this will likely usually be the case
+    let playerPawn = WHITE_PAWN;
+    let playerPieces = WHITE_PIECES;
+    let opponentPawn = BLACK_PAWN;
+    let opponentPieces = BLACK_PIECES;
+
+    if (!(isPlayingWhite)) {
+        playerPawn = BLACK_PAWN;
+        playerPieces = BLACK_PIECES;
+
+        opponentPawn = WHITE_PAWN
+        opponentPieces = WHITE_PIECES
+    }
+
+    const allSquares = Array.from(board.childNodes);
+  
+    let opponentPawnRow = allSquares.filter((square) => (square.id[1] == '7'));
+    let playerPawnRow = allSquares.filter((square) => (square.id[1] == '2'));
+
+    let opponentPieceRow = allSquares.filter((square) => (square.id[1] == '8'));
+    let playerPieceRow = allSquares.filter((square) => (square.id[1] == '1'));
+
+    // playerPawnRow/playerPieceRow and opponentPawnRow/opponentPieceRow should allhave length 8 
+    for (let i = 0; i < opponentPawnRow.length; i++) {
+        opponentPawnRow[i].innerHTML = opponentPawn;
+        playerPawnRow[i].innerHTML = playerPawn;
+
+        opponentPieceRow[i].innerHTML = opponentPieces[i];
+        playerPieceRow[i].innerHTML = playerPieces[i];
+    }
+}
 
 function colorSquare(square) {
     const GREEN = "#739552";
@@ -51,37 +87,6 @@ for (let row = 8; row > 0; row--) {
 
 roundCorners();
 placePieces();
-
-// TODO: Add functionality for placing the major pieces 
-function placePieces() {
-    let playerPawn;
-    let playerPieces
-    let opponentPawn;
-    let opponentPieces;
-
-    if (isPlayingWhite) {
-        playerPawn = WHITE_PAWN;
-        opponentPawn = BLACK_PAWN
-    }
-
-    else {
-        playerPawn = BLACK_PAWN;
-        opponentPawn = WHITE_PAWN;
-    }
-
-
-    const allSquares = Array.from(board.childNodes);
-  
-    opponentPawnRow = allSquares.filter((square) => (square.id[1] == '8'));
-    playerPawnRow = allSquares.filter((square) => (square.id[1] == '1'));
-
-    // playerPawnRow and opponentPawnRow should both have length 8 
-    for (let i = 0; i < opponentPawnRow.length; i++) {
-        opponentPawnRow[i].innerHTML = opponentPawn;
-        playerPawnRow[i].innerHTML = playerPawn;
-    }
-}
-
 
 
 
